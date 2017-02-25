27b8058676b86a86ed0c607147b667d34a058fda
Ömür Özkir <oemuer.oezkir@gmail.com>
2017-02-26 00:21:45 +0100

    code style [minor]

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static const Bool topbar            = True;     /* False means bottom bar */
 static const char *tags[] = { "USR", "DEV", "SYS", "COM" };
 
 static const Rule rules[] = {
-	/* class      instance    title       tags mask     isfloating   monitor */
+	/* class      instance        title       tags mask     isfloating   monitor */
 	{ "Gimp",       NULL,         NULL,       0,            True,        -1 },
 	{ "URxvt",     "URxvtFuzzy",  NULL,       0,            True,        -1 },
 	{ "Firefox",    NULL,         NULL,       1 << 8,       False,       -1 },
