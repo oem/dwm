4f7c386bafac2306bfd019f80ae165512525b482
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-10-10 10:49:32 +0200

    launches emacs and terminals in tag 1, browser in tag 2

--- config.h
+++ config.h
@@ -20,11 +20,13 @@ static const Bool topbar            = True;     /* False means bottom bar */
 static const char *tags[] = { "SRC", "USR", "SYS", "COM" };
 
 static const Rule rules[] = {
-	/* class      instance        title       tags mask     isfloating   monitor */
-	{ "Gimp",       NULL,         NULL,       0,            True,        -1 },
-	{ "URxvt",     "URxvtFuzzy",  NULL,       0,            True,        -1 },
-	{ "URxvt",     "URxvtRecord", NULL,       0,            True,        -1 },
-	{ "Firefox",    NULL,         NULL,       1 << 8,       False,       -1 },
+	/* class         instance       title       tags mask     isfloating   monitor */
+	{ "qutebrowser", NULL,          NULL,       1 << 1,       False,       -1 },
+	{ "URxvt",       "urxvt",       NULL,       1 << 0,       False,       -1 },
+	{ "Emacs",       NULL,          NULL,       1 << 0,       False,       -1 },
+	{ "URxvt",       "URxvtFuzzy",  NULL,       0,            True,        -1 },
+	{ "URxvt",       "URxvtRecord", NULL,       0,            True,        -1 },
+	{ "Firefox",      NULL,         NULL,       1 << 8,       False,       -1 },
 };
 
 /* layout(s) */
