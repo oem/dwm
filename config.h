e2310815e5dd9095180b9504059a1f2d274e605e
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-10-11 09:31:05 +0200

    brave browser will always open up in tag 3

--- config.h
+++ config.h
@@ -21,12 +21,13 @@ static const char *tags[] = { "SRC", "USR", "SYS", "COM" };
 
 static const Rule rules[] = {
 	/* class         instance       title       tags mask     isfloating   monitor */
-	{ "qutebrowser", NULL,          NULL,       1 << 1,       False,       -1 },
-	{ "URxvt",       "urxvt",       NULL,       1 << 0,       False,       -1 },
-	{ "Emacs",       NULL,          NULL,       1 << 0,       False,       -1 },
-	{ "URxvt",       "URxvtFuzzy",  NULL,       0,            True,        -1 },
-	{ "URxvt",       "URxvtRecord", NULL,       0,            True,        -1 },
-	{ "Firefox",      NULL,         NULL,       1 << 8,       False,       -1 },
+	{ "qutebrowser",   NULL,          NULL,       1 << 1,       False,       -1 },
+	{ "Brave-browser", NULL,          NULL,       1 << 2,       False,       -1 },
+	{ "URxvt",         "urxvt",       NULL,       1 << 0,       False,       -1 },
+	{ "Emacs",         NULL,          NULL,       1 << 0,       False,       -1 },
+	{ "URxvt",         "URxvtFuzzy",  NULL,       0,            True,        -1 },
+	{ "URxvt",         "URxvtRecord", NULL,       0,            True,        -1 },
+	{ "Firefox",       NULL,          NULL,       1 << 8,       False,       -1 },
 };
 
 /* layout(s) */
