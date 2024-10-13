0b76ce649af55826351bda884c606a9a28bae438
Ömür Özkir <oemuer.oezkir@gmail.com>
2024-10-13 10:50:52 +0200

    changes statusbar color to work nicer with gruvbox

--- config.h
+++ config.h
@@ -6,9 +6,9 @@ static const char font[] = "-*-tamsyn-medium-*-*-*-20-*-*-*-*-*-*-*";
 #define NUMCOLORS 3
 static const char colors[NUMCOLORS][ColLast][8] = {
     /* border     fg          bg */
-    {"#262626", "#757978", "#181825"}, /* 01 = normal */
-    {"#464646", "#C1C4BC", "#181825"}, /* 02 = selected */
-    {"#A04363", "#A04363", "#181825"}, /* 03 = urgent/warning */
+    {"#262626", "#757978", "#282828"}, /* 01 = normal */
+    {"#464646", "#C1C4BC", "#282828"}, /* 02 = selected */
+    {"#A04363", "#A04363", "#282828"}, /* 03 = urgent/warning */
 };
 
 static const unsigned int borderpx = 1; /* border pixel of windows */
